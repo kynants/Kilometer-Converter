e9ba996dd7298bcb9b8439e642f9f830ec35e6ca
Kynan Stevenson <36722419+kynants@users.noreply.github.com>
2018-12-29 01:01:42 -0500

    Add files via upload
    Initial Commit

---
+++ Kilometer-Converter/Kilometer-Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kilometer_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            double kilometers, miles;
+
+            // User Input
+            Console.Write("Enter a distance in kilometers: ");
+            kilometers = Convert.ToDouble(Console.ReadLine());
+
+            // Calculations
+            miles = kilometers * 0.6214;
+
+            // Final Result
+            Console.WriteLine(kilometers + " km is " + miles + " mi.");
+        }
+    }
+}
